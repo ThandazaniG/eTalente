ff75aabb908d23969244fcda209d33dfc23f1e96
thandazanig <thandazanigwampa99@gmail.com>
2023-07-06 18:11:23 +0200

    "Added and updated exceptions"

---
+++ main/java/com/eviro/assessment/grad001/ThandazaniGwampa/exception/ApplicationConstrainViolationException.java
@@ -0,0 +1,15 @@
+package com.eviro.assessment.grad001.ThandazaniGwampa.exception;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+
+import java.util.Set;
+public class ApplicationConstrainViolationException extends ConstraintViolationException {
+
+    public ApplicationConstrainViolationException(String message, Set<? extends ConstraintViolation<?>> constraintViolations) {
+        super(message, constraintViolations);
+    }
+    public ApplicationConstrainViolationException(Set<? extends ConstraintViolation<?>> constraintViolations) {
+        super(constraintViolations);
+    }
+
+}
---
+++ main/java/com/eviro/assessment/grad001/ThandazaniGwampa/exception/ImageNotValid.java
@@ -0,0 +1,7 @@
+package com.eviro.assessment.grad001.ThandazaniGwampa.exception;
+
+public class ImageNotValid extends RuntimeException {
+    public ImageNotValid(String message) {
+        super(message);
+    }
+}
